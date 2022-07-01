036b0b8bcf3652d15d65c63c622bc005307213de
Playhawks <79812619+Playhawks@users.noreply.github.com>
2022-07-01 11:15:00 +0200

    Alle Items registriert und Übersetzungen erstellt

--- src/main/java/com/mio/core/item/ModItems.java
+++ src/main/java/com/mio/core/item/ModItems.java
@@ -12,12 +12,77 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MioCore.MOD_ID);
 
-    public static final RegistryObject<Item> AluminiumIngot = ITEMS.register("aluminium_ingot" ,
+    public static final RegistryObject<Item> ALUMINIUM_INGOT = ITEMS.register("aluminium_ingot" ,
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
-    public static final RegistryObject<Item> AluminiumNugget = ITEMS.register("aluminium_nugget" ,
+    public static final RegistryObject<Item> ALUMINIUM_NUGGET = ITEMS.register("aluminium_nugget" ,
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> RAW_ALUMINIUM = ITEMS.register("raw_aluminium" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> ARDITE_INGOT = ITEMS.register("ardite_ingot" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> ARDITE_NUGGET = ITEMS.register("ardite_nugget" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_ARDITE = ITEMS.register("raw_ardite" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> BLEI_INGOT = ITEMS.register("blei_ingot" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> BLEI_NUGGET = ITEMS.register("blei_nugget" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_BLEI = ITEMS.register("raw_blei" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> ENHANCED_BONE_MEAL = ITEMS.register("enhanced_bone_meal" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> NICKEL_INGOT = ITEMS.register("nickel_ingot" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> NICKEL_NUGGET = ITEMS.register("nickel_nugget" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_NICKEL = ITEMS.register("raw_nickel" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> OSMIUM_INGOT = ITEMS.register("osmium_ingot" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> OSMIUM_NUGGET = ITEMS.register("osmium_nugget" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_OSMIUM = ITEMS.register("raw_osmium" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> PLATIN_INGOT = ITEMS.register("platin_ingot" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> PLATIN_NUGGET = ITEMS.register("platin_nugget" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_PLATIN = ITEMS.register("raw_platin" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> SILVER_NUGGET = ITEMS.register("silver_nugget" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_SILVER = ITEMS.register("raw_silver" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> URAN_INGOT = ITEMS.register("uran_ingot" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_URAN = ITEMS.register("raw_uran" ,
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
